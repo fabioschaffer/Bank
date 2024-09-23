9fefd66bcb43e5bfa42f354f149c45f24495d433
Fabio Schaffer <fabioschaffer@gmail.com>
2024-09-23 16:06:57 -0300

    Update CreateObjects.sql

--- DataBase/CreateObjects.sql
+++ DataBase/CreateObjects.sql
@@ -1,12 +1,13 @@
-CREATE TABLE City {
-Id		integer,
-Name	varchar(128),
+CREATE TABLE City(
+	Id			integer 		NOT NULL,
+	Name		varchar(128)	NOT NULL,
+	CONSTRAINT 	PK_City 		PRIMARY KEY (Id)
+);
 
-}
-
-
-CREATE TABLE Person (
-Id 		integer,
-Name 	varchar(120),
-City
-)
+CREATE TABLE Person(
+	Id 			integer			NOT NULL,
+	Name 		varchar(128)	NOT NULL,
+	CityId		integer			NOT NULL,
+	Phone		numeric(11)		NULL,
+	CONSTRAINT 	FK_Person_City	FOREIGN KEY (CityId) REFERENCES City(Id)
+);
